005eca63f544b4737dd9f1835daf7caaf67ca34d
Qunainb <waniqunain@gmail.com>
2025-05-30 23:00:19 +0530

    action functions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import EventDetailPage, {
   loader as eventDetailLoader,
 } from "./pages/EventDetail";
-import NewEventPage from "./pages/NewEvent";
+import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import RootLayout from "./pages/Root";
 import EventsRootPage from "./pages/EventsRoot";
@@ -43,6 +43,7 @@ function App() {
             {
               path: "new",
               element: <NewEventPage />,
+              action: newEventAction,
             },
           ],
         },
